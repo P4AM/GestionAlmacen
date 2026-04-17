412526e24cdd1e986ed413e32d8f6cbe1d4374a8
P4AM <agustin.pmall@gmail.com>
2026-04-16 20:33:53 -0500

    Creacion de el Controlador,Vista, y Modelo del programa Gestion de Almacen

---
+++ GestionAlmacen/src/main/java/com/mycompany/gestionalmacen/GestionAlmacen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestionalmacen;
+
+/**
+ *
+ * @author A4pga
+ */
+public class GestionAlmacen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
